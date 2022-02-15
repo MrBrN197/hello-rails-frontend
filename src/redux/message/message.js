b369e5398218cc90916fa15c9de41b81864c4a36
brianorora <brianorora@gmail.com>
2022-02-15 16:36:27 +0300

    Update README

--- src/redux/message/message.js
+++ src/redux/message/message.js
@@ -1,7 +1,6 @@
 const SET_MESSAGE = 'hello-rails-front-end/message/SET_MESSAGE';
 
 const reducer = (state = '', action) => {
-  console.log('setting state => ', action.payload);
   switch (action.type) {
     case SET_MESSAGE:
       return action.payload;
@@ -15,7 +14,6 @@ const URL = 'http://localhost:3001/api/messages';
 export const setMessage = () => async (dispatch) => {
   const resp = await fetch(URL);
   const data = await resp.json();
-  console.log('dispatching');
   dispatch({
     type: SET_MESSAGE,
     payload: data.message,
